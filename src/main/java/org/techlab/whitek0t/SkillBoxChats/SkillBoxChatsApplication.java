cf38887a6842cd3b4c5bfbfdbddaddc049a12590
SAM <nety@nety.ru>
2021-05-22 14:28:35 +0400

    first commit

---
+++ src/main/java/org/techlab/whitek0t/SkillBoxChats/SkillBoxChatsApplication.java
@@ -0,0 +1,13 @@
+package org.techlab.whitek0t.SkillBoxChats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillBoxChatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillBoxChatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/techlab/whitek0t/SkillBoxChats/SkillBoxChatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.techlab.whitek0t.SkillBoxChats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillBoxChatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
